f2158f7e308c7ac2fa6889b7202f97a9cc412b02
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2024-12-26 09:57:17 +0700

    delete flashcard controller/view, edit js in course view

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartCards.DTOs.Flashcard;
+
+namespace SmartCards.Controllers
+{
+	[Authorize]
+	public class CourseController : Controller
+	{
+		//[Route("/create-course")]
+		public IActionResult Create(List<FlashcardDTO>? flashcardsDTO)
+		{
+			if (flashcardsDTO != null && flashcardsDTO.Count != 0)
+			{
+				foreach (var item in flashcardsDTO)
+				{
+					Console.WriteLine(item.Term);
+				}
+			}
+
+			return View();
+		}
+	}
+}
