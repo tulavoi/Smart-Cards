1c9cb8cc7e77a1204a79b4f0f5a963598e68350c
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2025-01-02 18:01:35 +0700

    reassign value to Title, Term, Definition input when modal state is not valid

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -39,11 +39,39 @@ namespace SmartCards.Controllers
         [HttpPost("/create-course")]
         public async Task<IActionResult> Create(CreateCourseRequestDTO courseDTO)
         {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
+            string errorMessage = this.GetErrorMessage(courseDTO);
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                TempData["ErrorMessage"] = errorMessage;
+                ViewBag.EditPermissions = await _permissionRepo.GetAllAsync(new PermissionQueryObject { IsEdit = true });
+                ViewBag.ViewPermissions = await _permissionRepo.GetAllAsync(new PermissionQueryObject { IsEdit = false });
+                ViewBag.Languages = await _languageRepo.GetAllAsync(new LanguageQueryObject { SortBy = "Name" });
+                return View(courseDTO);
+            }
+
             var course = courseDTO.ToCourseFromCreateDTO(this.UserId);
             await _courseRepo.CreateAsync(course, courseDTO.ViewPermissionId, courseDTO.EditPermissionId);
 
             return CreatedAtAction(nameof(GetById), new { id = course.Id }, course.ToCourseDTO());
+        }
+
+        private string GetErrorMessage(CreateCourseRequestDTO courseDTO)
+        {
+            List<string> messages = new List<string>();
+            if (string.IsNullOrEmpty(courseDTO.Title)) // Nếu không có tiêu đề học phần
+                messages.Add("nhập tiêu đề ");
+
+            if (courseDTO.Flashcards.Count < 3) // Nếu ít hơn 3 flashcard
+				messages.Add("nhiều hơn hai thẻ ");
+
+            if (courseDTO.Flashcards[0].TermLanguageId == 0 || courseDTO.Flashcards[0].DefiLanguageId == 0) // Nếu chưa chọn term or definition language
+                messages.Add("một ngôn ngữ cho thuật ngữ và một ngôn ngữ cho định nghĩa ");
+
+            // Nếu như messages không có phần tử nào thì combinedMessage là chuỗi rỗng
+            string combinedMessage = messages.Count != 0 ? $"bạn cần {string.Join(", ", messages)} để tạo 1 học phần." : "";
+
+            return combinedMessage;
         }
 
         [HttpGet("{id:int}")]
